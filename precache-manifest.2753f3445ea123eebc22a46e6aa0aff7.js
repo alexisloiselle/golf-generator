cbabc1ffaf8700b8f6151e6656dac8a3182fd0b8
Alexis Loiselle <alexis@t-b.com>
2020-05-23 13:06:15 -0400

    Updates

---
+++ precache-manifest.2753f3445ea123eebc22a46e6aa0aff7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ad788cac0878062c007bd4a6c0ce20f",
+    "url": "/golf-generator/index.html"
+  },
+  {
+    "revision": "7203e2c620041f5a313a",
+    "url": "/golf-generator/static/css/main.2112e917.chunk.css"
+  },
+  {
+    "revision": "5257d97db00b0fb329e1",
+    "url": "/golf-generator/static/js/2.005967ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/golf-generator/static/js/2.005967ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7203e2c620041f5a313a",
+    "url": "/golf-generator/static/js/main.b3ca4550.chunk.js"
+  },
+  {
+    "revision": "390d9e8955e89842e9ca",
+    "url": "/golf-generator/static/js/runtime-main.6cfdf871.js"
+  }
+]);
